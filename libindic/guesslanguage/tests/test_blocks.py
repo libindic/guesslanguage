4073cf7072432d2b7923eb0fabc098b5aa70209a
Balasankar C <balasankarc@autistici.org>
2017-09-08 09:48:01 +0530

    Convert to namespace package

--- libindic/guesslanguage/tests/test_blocks.py
+++ libindic/guesslanguage/tests/test_blocks.py
@@ -1,4 +1,4 @@
-''' Copyright (c) 2008, Kent S Johnson 
+''' Copyright (c) 2008, Kent S Johnson
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -12,21 +12,32 @@
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+    Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301 USA
 '''
 
-import unittest
+from testtools import TestCase
 
-from blocks import unicodeBlock
+from ..blocks import unicodeBlock
 
-class blocks_test(unittest.TestCase):
+
+class BlocksTest(TestCase):
+
+    def setUp(self):
+        super(BlocksTest, self).setUp()
+
+    def assertBlock(self, name, c):
+        c = unichr(c)
+        block = unicodeBlock(c)
+        self.assertEquals(name, unicodeBlock(c),
+                          '%s != %s for %r' % (name, block, c))
+
     def test_unicodeBlock(self):
         for c in range(128):
             self.assertBlock('Basic Latin', c)
-        
+
         for c in range(0x80, 0x180) + range(0x250, 0x2B0):
             self.assertBlock('Extended Latin', c)
-        
+
         self.assertBlock('Thai', 0xE00)
         self.assertBlock('Thai', 0xE7F)
         self.assertBlock('Lao', 0xE80)
@@ -34,16 +45,3 @@ class blocks_test(unittest.TestCase):
         self.assertBlock('Tibetan', 0xF00)
         self.assertBlock('Tibetan', 0xFFF)
         self.assertBlock('Cyrillic', 0x421)
-        
-    def assertBlock(self, name, c):
-        c = unichr(c)
-        block = unicodeBlock(c)
-        self.assertEquals(name, unicodeBlock(c), '%s != %s for %r' % (name, block, c))
-            
-
-    def setUp(self):
-        pass
-
-    
-if __name__ == '__main__':
-    unittest.main()
